3396a2820b548e48ef7726989825653355d39b8b
Hayden Atchley <atchley.sha@protonmail.com>
2023-09-05 10:53:16 -0600

    fix erroneous removal of function

--- R/data_helpers.R
+++ R/data_helpers.R
@@ -1,5 +1,13 @@
 # Functions to help with reading in and cleaning data
 
+pivot_tbm_edges <- function(edges) {
+  
+  read_csv(edges) %>% 
+    mutate(across(where(is.numeric), ~ ifelse(.x < 0, 0, .x))) %>% 
+    pivot_longer(-c(from, to), names_to = "mode", values_to = "number")
+  
+}
+
 get_ex_zones <- function(distance_omx){
   read_all_omx(distance_omx, "HBW") %>%
     rename(distance = HBW) %>%
